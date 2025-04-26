faf686281f0a970c7b9d172dc1e06c078a76290d
Ideabees <ideabees.tech@gmail.com>
2025-04-26 21:43:29 +0530

    cors enabled

--- wms-app/internal/routes/router.go
+++ wms-app/internal/routes/router.go
@@ -4,10 +4,19 @@ import (
 	"wms-app/internal/controllers"
 	"wms-app/internal/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func SetupRoutes() *gin.Engine {
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"http://localhost:3000"} // Replace with your client's origin(s)
+    config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+    config.AllowHeaders = []string{"Origin", "Authorization", "Content-Type"}
+
+    r.Use(cors.New(config))
+
 	r.POST("/api/register", controllers.Register)
 
 	r.POST("/api/login", controllers.Login)
