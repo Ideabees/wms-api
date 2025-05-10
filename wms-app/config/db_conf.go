ec6aed9ccaa4e2fdab6b352dd4e88d8c2a78a725
Ideabees <ideabees.tech@gmail.com>
2025-05-10 23:41:41 +0530

    cleanup

--- wms-app/config/db_conf.go
+++ wms-app/config/db_conf.go
@@ -19,9 +19,12 @@ func InitDB() {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		dbHost, dbUser, dbPassword, dbName, dbPort)
 	//dsn := "host=localhost user=wmsuser password=wmspassword dbname=wmsdb port=5432 sslmode=disable"
+	fmt.Println("Connection string is----")
+	fmt.Println(dsn)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to DB:", err)
+		log.Fatal("Checking connection again")
 	}
 	DB = db
 }
